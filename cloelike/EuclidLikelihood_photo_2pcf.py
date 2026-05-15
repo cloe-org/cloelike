3bce5b73a6f42d450dfc946dc403b818cf881ac7
NastassiaG <107264848+NastassiaG@users.noreply.github.com>
2026-05-15 11:01:24 +0100

    include_rsd option added

--- cloelike/EuclidLikelihood_photo_2pcf.py
+++ cloelike/EuclidLikelihood_photo_2pcf.py
@@ -163,6 +163,7 @@ class GCphMixin:
             self.zs,
             nuisance_params={key: parameters[key] for key in self.full_pos_keys},
             galaxy_bias_model="poly",
+            include_rsd=self.settings.get("include_rsd", False),
         )
         cf_all_th = AngularCorrelationFunctionWigner(
             AngularTwoPoint(pos, pos), self.ells_integration, nlp.k
@@ -247,6 +248,7 @@ class GGLMixin:
             self.zs,
             nuisance_params={key: parameters[key] for key in self.full_pos_keys},
             galaxy_bias_model="poly",
+            include_rsd=self.settings.get("include_rsd", False),
         )
         she = ShearTracer(
             nlp,
