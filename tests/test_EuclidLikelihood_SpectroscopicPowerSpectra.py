5c04a93b40edae89c1cacb5b652aab2b4717c1d9
Andrea Pezzotta <andrea.pezzotta@inaf.it>
2026-06-30 09:44:11 +0200

    Test

--- tests/test_EuclidLikelihood_SpectroscopicPowerSpectra.py
+++ tests/test_EuclidLikelihood_SpectroscopicPowerSpectra.py
@@ -266,7 +266,7 @@ def test_likelihood_value_with_AM(data_setup, settings_setup, AM_priors_setup):
 
     computed_loglike = likelihood.loglike_AM(parameters)
 
-    expected_loglike = -113.5673
+    expected_loglike = -114.5673
     assert computed_loglike == pytest.approx(expected_loglike, rel=1e-5), (
         f"Expected log-likelihood to be approximately {expected_loglike}, "
         f"but got {computed_loglike}"
