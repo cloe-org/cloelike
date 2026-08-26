2fa79c50cbbdd32e6fb1b2127ceae4131aed7550
Andrea Pezzotta <andrea.pezzotta@inaf.it>
2026-08-26 16:42:29 +0200

    First round of review

--- tests/test_EuclidLikelihood_GCspectro_xils.py
+++ tests/test_EuclidLikelihood_GCspectro_xils.py
@@ -268,18 +268,18 @@ def test_hartlap_factor(data_setup, settings_setup):
         Background=CAMBBackground,
         SpectroPower=CometEFT_SpectroPower,
     )
-    N_d = like_no_hartlap.masked_covariance_matrix.shape[0]
-    N_s = N_d + 200
+    num_data_points = like_no_hartlap.masked_covariance_matrix.shape[0]
+    num_mocks = num_data_points + 200
 
     like_hartlap = EuclidLikelihood_GCspectro_xils(
         data=data_setup,
         settings=settings_setup,
         Background=CAMBBackground,
         SpectroPower=CometEFT_SpectroPower,
-        N_s=N_s,
+        num_mocks=num_mocks,
     )
 
-    expected_factor = (N_s - N_d - 2) / (N_s - 1)
+    expected_factor = (num_mocks - num_data_points - 2) / (num_mocks - 1)
     np.testing.assert_allclose(
         like_hartlap.inverse_masked_covariance_matrix,
         expected_factor * like_no_hartlap.inverse_masked_covariance_matrix,
@@ -287,7 +287,7 @@ def test_hartlap_factor(data_setup, settings_setup):
 
 
 def test_hartlap_factor_with_few_realisations(data_setup, settings_setup):
-    N_d = EuclidLikelihood_GCspectro_xils(
+    num_data_points = EuclidLikelihood_GCspectro_xils(
         data=data_setup,
         settings=settings_setup,
         Background=CAMBBackground,
@@ -300,5 +300,5 @@ def test_hartlap_factor_with_few_realisations(data_setup, settings_setup):
             settings=settings_setup,
             Background=CAMBBackground,
             SpectroPower=CometEFT_SpectroPower,
-            N_s=N_d,
+            num_mocks=num_data_points,
         )
